4bfea99a5c80ac934e4ef0c0ed8145b2d5523fe6
tarcisiocorte <tarcisiocorte@gmail.com>
2017-08-18 14:39:56 -0300

    added the db.php file

---
+++ config/db.php
@@ -0,0 +1,12 @@
+<?php
+return [
+  'development' => [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'php_fin',
+    'username' => 'root',
+    'password' => '1234.Abcd',
+    'charset' => 'utf8',
+    'collection' => 'utf8_unicode_ci'
+  ]
+];
