ac17e648fafa9859de74c2835801223ee0934095
tarcisiocorte <tarcisiocorte@gmail.com>
2017-08-29 21:40:48 -0300

    created category-costs router file

--- public/index.php
+++ public/index.php
@@ -28,62 +28,6 @@ $app->get('/home/{name}/{id}', function(ServerRequestInterface $request){
     return $response;
 });
 
-$app->get('/category-costs', function() use($app){
-    $view = $app->service('view.renderer');
-    $meuModel = new \SONFin\Models\CategoryCost();
-    $categories = $meuModel->all();
-    return $view->render('category-costs/list.html.twig', [
-        'categories' => $categories
-    ]);
-});
-
-$app
-    ->get('/category-costs', function() use($app){
-        $meuModel = new CategoryCost();
-        $categories = $meuModel->all();
-        $view = $app->service('view.renderer');
-        return $view->render('category-costs/list.html.twig',[
-            'categories' => $categories
-        ]);
-    }, 'category-costs.list')
-    ->get('/category-costs/new', function() use($app){
-        $view = $app->service('view.renderer');
-        return $view->render('category-costs/create.html.twig');
-    }, 'category-costs.new')
-    ->get('/category-costs/{id}/edit', function(ServerRequestInterface $request) use($app){
-        $view = $app->service('view.renderer');
-        $id = $request->getAttribute('id');
-        $category = \SONFin\Models\CategoryCost::findOrFail($id);
-        return $view->render('category-costs/edit.html.twig', [
-            'category' => $category
-        ]);
-    }, 'category-costs.edit')
-    ->get('/category-costs/{id}/show', function(ServerRequestInterface $request) use($app){
-        $view = $app->service('view.renderer');
-        $id = $request->getAttribute('id');
-        $category = \SONFin\Models\CategoryCost::findOrFail($id);
-        return $view->render('category-costs/show.html.twig', [
-            'category' => $category
-        ]);
-    }, 'category-costs.show')
-    ->get('/category-costs/{id}/delete', function(ServerRequestInterface $request) use($app){
-        $id = $request->getAttribute('id');
-        $category = \SONFin\Models\CategoryCost::findOrFail($id);
-        $category->delete();
-        return $app->route('category-costs.list');
-    }, 'category-costs.delete')
-    ->post('/category-costs/{id}/update', function(ServerRequestInterface $request) use($app) {
-        $id = $request->getAttribute('id');
-        $category = \SONFin\Models\CategoryCost::findOrFail($id);
-        $data = $request->getParsedBody();
-        $category->fill($data);
-        $category->save();
-        return $app->route('category-costs.list');
-    }, 'category-costs.update')
-    ->post('/category-costs/store', function(ServerRequestInterface $request) use($app){
-        $data = $request->getParsedBody();
-        SONFin\Models\CategoryCost::create($data);
-        return $app->route('category-costs.list');
-    }, 'category-costs.store');
+require_once __DIR__ . '/../src/controllers/category-costs.php';
 
 $app->start();
